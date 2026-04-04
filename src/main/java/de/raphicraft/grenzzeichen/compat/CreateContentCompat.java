ad155ce39fd04b80fe07ef752d5c65fa83ffc3a4
Jannikca <Clausnitzer762@gmail.com>
2026-04-04 21:40:15 +0200

    fully patched to create 6.0.8.0+build.1734-mc1.20.1

--- src/main/java/de/raphicraft/grenzzeichen/compat/CreateContentCompat.java
+++ src/main/java/de/raphicraft/grenzzeichen/compat/CreateContentCompat.java
@@ -1,7 +1,10 @@
 package de.raphicraft.grenzzeichen.compat;
 
+
 import com.simibubi.create.AllInteractionBehaviours;
 import com.simibubi.create.AllMovementBehaviours;
+import com.simibubi.create.api.behaviour.interaction.MovingInteractionBehaviour;
+import com.simibubi.create.api.behaviour.movement.MovementBehaviour;
 import com.simibubi.create.content.contraptions.actors.trainControls.ControlsInteractionBehaviour;
 import de.raphicraft.grenzzeichen.block.ModBlocks;
 import de.raphicraft.grenzzeichen.compat.create.FuehrerstandMovementBehaviour;
@@ -11,8 +14,8 @@ public class CreateContentCompat {
 //    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Grenzzeichen.MOD_ID);
 
     public static void register() {
-        AllMovementBehaviours.registerBehaviour(ModBlocks.FUEHRERSTAND, new FuehrerstandMovementBehaviour());
-        AllInteractionBehaviours.registerBehaviour(ModBlocks.FUEHRERSTAND, new ControlsInteractionBehaviour());
+        MovementBehaviour.REGISTRY.register(ModBlocks.FUEHRERSTAND, new FuehrerstandMovementBehaviour());
+        MovingInteractionBehaviour.REGISTRY.register(ModBlocks.FUEHRERSTAND, new ControlsInteractionBehaviour());
 
 //        REGISTRATE.register();
     }
--- src/main/java/de/raphicraft/grenzzeichen/compat/create/FuehrerstandMovementBehaviour.java
+++ src/main/java/de/raphicraft/grenzzeichen/compat/create/FuehrerstandMovementBehaviour.java
@@ -1,17 +1,12 @@
 package de.raphicraft.grenzzeichen.compat.create;
 
-import com.jozufozu.flywheel.core.virtual.VirtualRenderWorld;
 import com.simibubi.create.content.contraptions.actors.trainControls.ControlsMovementBehaviour;
 import com.simibubi.create.content.contraptions.behaviour.MovementContext;
 import com.simibubi.create.content.contraptions.render.ContraptionMatrices;
+import com.simibubi.create.foundation.virtualWorld.VirtualRenderWorld;
 import net.minecraft.client.render.VertexConsumerProvider;
 
 public class FuehrerstandMovementBehaviour extends ControlsMovementBehaviour {
-
-    @Override
-    public boolean renderAsNormalBlockEntity() {
-        return true;
-    }
 
     @Override
     public void renderInContraption(MovementContext context, VirtualRenderWorld renderWorld, ContraptionMatrices matrices, VertexConsumerProvider buffer) {
